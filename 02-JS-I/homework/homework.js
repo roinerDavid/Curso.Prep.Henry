91111a15b9751a9b3c71b80e1fc35941565fbc37
RoinerDavid <fullvictoria01@gmail.com>
2022-11-22 13:09:00 -0500

    bool

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = "Hola";
 const nuevoNum = null;
 
 // Crea una variable booleana:
-const nuevoBool = True;
+const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - null === 5;
